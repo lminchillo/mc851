5d2c321867cc34c1b65c36a155b1875c70a827d1
lminchillo <lminchillo2@gmail.com>
2013-05-10 20:33:26 -0300

    Created new launcher activity, deleted an old unecessary activity, updated app default style

---
+++ src/lais/mc851/Splashscreen.java
@@ -0,0 +1,31 @@
+package lais.mc851;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.app.Activity;
+
+public class Splashscreen extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splashscreen);
+		
+		new CountDownTimer(2000,1999)
+		{
+			@Override
+			public void onTick(long millisUntilFinished)
+			{
+				
+			}
+			
+			@Override
+			public void onFinish()
+			{
+				//startActivity(new Intent(getApplicationContext(),StartScreen.class));
+				finish();
+			}
+		};
+	}
+}
