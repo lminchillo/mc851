0b5fd2f2e574dc8481926dc90df7f65499815339
lminchillo <lminchillo2@gmail.com>
2013-05-10 21:55:52 -0300

    Added a new activity (Settings)

---
+++ src/lais/mc851/Settings.java
@@ -0,0 +1,14 @@
+package lais.mc851;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Settings extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+}
--- src/lais/mc851/StartScreen.java
+++ src/lais/mc851/StartScreen.java
@@ -3,9 +3,13 @@ package lais.mc851;
 import java.util.ArrayList;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.app.Activity;
+import android.content.Intent;
 
 public class StartScreen extends Activity
 {
@@ -19,12 +23,26 @@ public class StartScreen extends Activity
 		arrayList.add(getResources().getString(R.string.startscreen_take_bus));
 		arrayList.add(getResources().getString(R.string.startscreen_saved_routes));
 		arrayList.add(getResources().getString(R.string.startscreen_saved_adresses));
-		arrayList.add(getResources().getString(R.string.startscreen_my_cupons));
+		arrayList.add(getResources().getString(R.string.startscreen_my_coupons));
 		arrayList.add(getResources().getString(R.string.startscreen_config));
 		
 		ListView listView = (ListView) findViewById(R.id.startscreen_listview);
 		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,arrayList);
 		listView.setAdapter(arrayAdapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			{
+				position++;
+				//if (position==1) startActivity(new Intent(getApplicationContext(),GetRoute.class));
+				//else if (position==2) startActivity(new Intent(getApplicationContext(),Routes.class));
+				//else if (position==3) startActivity(new Intent(getApplicationContext(),Adresses.class));
+				//else if (position==4) startActivity(new Intent(getApplicationContext(),Coupons.class));
+				if (position==5) startActivity(new Intent(getApplicationContext(),Settings.class));
+			}
+		});
 		
 	}
 }
