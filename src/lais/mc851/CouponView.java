6f022c4cd2e13c5a2269a0a1336d16f396dc0d50
rodrigolobo <rodrigo.lobo.costa@gmail.com>
2013-06-18 16:35:46 -0300

    implementing the listview for Coupons; some temporary hardcoded coupons added

--- src/lais/mc851/CouponView.java
+++ src/lais/mc851/CouponView.java
@@ -1,14 +1,23 @@
 package lais.mc851;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CouponView extends Activity 
 {
 	private Button buttonDelete;
+	
+	private String couponName = null;
+	private String couponDescription = null;
+	private TextView textViewName = null;
+	private TextView textViewDescription = null;
+	private boolean savedCoupon = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -16,18 +25,51 @@ public class CouponView extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_coupon_view);
 
-		initializeButtons();
+		Bundle b = getIntent().getExtras();
+		if (!b.getString("couponName", "").equals("") && !b.getString("couponDescription", "").equals(""))
+		{
+			savedCoupon = true;
+			couponName = b.getString("couponName", "");
+			couponDescription = b.getString("couponDescription", "");
+			System.out.println("name: " + couponName);
+			System.out.println("description: " + couponDescription);
+		}
+		
+		initializeWidgets();
 	}
 	
-	private void initializeButtons()
+	private void initializeWidgets()
 	{
+		textViewName = (TextView) findViewById(R.id.coupons_details_text_view_title);
+		textViewName.setText(couponName);
+		
+		textViewDescription = (TextView) findViewById(R.id.coupons_details_text_view_content);
+		textViewDescription.setText(couponDescription);
+		
 		buttonDelete = (Button) findViewById(R.id.coupons_details_button_delete);
 		buttonDelete.setOnClickListener(new OnClickListener()
 		{
 			@Override
-			public void onClick(View v) 
+			public void onClick(View v)
 			{
-				
+				if(savedCoupon){
+						
+						if (RouteManager.removeAddress(couponName))
+						{
+							Toast.makeText(getApplicationContext(), "Cupom deletado", Toast.LENGTH_SHORT).show();
+							startActivity(new Intent(getApplicationContext(),Coupons.class));		
+							finish();
+						}
+						else
+						{
+							Toast.makeText(getApplicationContext(), "Erro ao deletar o cupom", Toast.LENGTH_SHORT).show();
+						}
+					}
+				else
+				{
+					Toast.makeText(getApplicationContext(), "Esse cupom não foi salvo ainda", Toast.LENGTH_SHORT).show();
+				}
+					
 			}
 		});
 	}
--- src/lais/mc851/Coupons.java
+++ src/lais/mc851/Coupons.java
@@ -1,36 +1,112 @@
 package lais.mc851;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class Coupons extends Activity
-{
-	// TODO: remover depois
-	private Button buttonDummy;
-	
+public class Coupons extends Activity {
+	private ListView couponListView = null;
+	private ArrayList<String> couponList = null;
+	private MyArrayAdapter adapter = null;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_coupons);
-		
-		initializeButtons();
+
+		initialize();
 	}
-	
-	private void initializeButtons()
-	{
-		buttonDummy = (Button) findViewById(R.id.coupons_button_dummy);
-		buttonDummy.setOnClickListener(new OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
+
+	@Override
+	protected void onResume() {
+		initializeListView();
+		super.onResume();
+	}
+
+	private void initialize() {
+		if (couponListView == null) {
+			couponListView = (ListView) findViewById(R.id.coupons_listview);
+		}
+
+		TextView noCoupons = (TextView) findViewById(R.id.coupons_text_view_no_coupons);
+
+		// TODO: usar CouponManager, ao inves de hardcoded
+		couponList = new ArrayList<String>();
+		couponList.add("Coxinha|Desconto de 15% na compra de Coxinha!");
+		couponList.add("Quibe|Desconto de 10% na compra de Quibe!");
+		couponList.add("Espinafre|Desconto de 90% na compra de Espinafre, mas não vale para os dias primos do mês, nem para os dias em que o presidente Barack Obama aparece na televisão do México");
+
+		if (couponList == null || couponList.size() == 0) {
+			noCoupons.setVisibility(View.VISIBLE);
+		} else {
+			noCoupons.setVisibility(View.INVISIBLE);
+			initializeListView();
+		}
+	}
+
+	private void initializeListView() {
+		if (adapter == null) {
+			adapter = new MyArrayAdapter(getApplicationContext(),
+					R.layout.listview_item_simple, couponList);
+			couponListView.setAdapter(adapter);
+		}
+		adapter.notifyDataSetChanged();
+	}
+
+	private class MyArrayAdapter extends ArrayAdapter<String> {
+		LayoutInflater inflater;
+
+		public MyArrayAdapter(Context ctx, int resourceId,
+				ArrayList<String> objects) {
+			super(ctx, resourceId, objects);
+			inflater = LayoutInflater.from(getApplicationContext());
+		}
+
+		@Override
+		public View getView(final int position, View convertView,
+				ViewGroup parent) {
+			convertView = inflater.inflate(R.layout.listview_item_simple, null);
+
+			System.out.println("Posicao: " + position);
+			String coupon = couponList.get(position);
+			System.out.println("coupon: " + coupon);
+			String s[] = coupon.split("\\|");
+			final String couponName = s[0];
+			final String couponDescription = s[1];
+
+			// limitando o tamanho ao apresentar na listview
+			String auxDescription = couponDescription;
+			if(auxDescription.length() > 40)
 			{
-				startActivity(new Intent(getApplicationContext(), CouponView.class));
+				auxDescription = auxDescription.substring(0, 37) + "...";
 			}
-		});
+			TextView tv = (TextView) convertView
+					.findViewById(R.id.listview_item_simple_text);
+			tv.setText("\n" + couponName + "\n" + auxDescription + "\n");
+
+			convertView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(getApplicationContext(),
+							CouponView.class);
+					intent.putExtra("couponName", couponName);
+					intent.putExtra("couponDescription", couponDescription);
+					startActivity(intent);
+				}
+			});
+
+			return convertView;
+		}
+
 	}
 }
